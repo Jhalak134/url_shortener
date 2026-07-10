04fd98a9cfa0513012146d7271185c4216343f48
Jhalak134 <mittaljhalak524@gmail.com>
2026-07-10 14:40:11 +0530

    made changes in click.js

--- backend/src/queues/click.queue.js
+++ backend/src/queues/click.queue.js
@@ -3,7 +3,14 @@ const redisClient = require('../config/redis');
 
 // Initialize BullMQ click queue
 const clickQueue = new Queue('click-events', {
-  connection: redisClient
+  connection: redisClient,
+  defaultJobOptions: {
+    attempts: 5,
+    backoff: {
+      type: 'exponential',
+      delay: 1000,
+    },
+  }
 });
 
 module.exports = clickQueue;
